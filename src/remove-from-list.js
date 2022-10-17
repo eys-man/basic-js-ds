51763c3d135aca3cff2940dc1ba3fe5050906a40
eys-man <kapuec2010@gmail.com>
2022-10-18 01:15:26 +0300

    feat: add task remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+//function removeKFromList(/* l, k */) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+//}
+
+function removeKFromList( l, k )
+{
+  let head=l;
+  let current=l;
+  let prev = null; // предыдущий узел
+  
+  console.log('символ ' + k)
+  while( current ) // пробегаем по всему списку
+  {
+    console.log('символ очередного элемента ' + current.value);
+
+    if( current.value === k ) // удалить узел
+    {
+      if( prev ) prev.next=current.next; // если удалялся не первый элемент
+      else head=current.next; // если удалили первый, то голова на следующий
+    }
+    else 
+     prev=current;
+    
+    current=current.next;
+  }
+  return head;
 }
 
 module.exports = {
